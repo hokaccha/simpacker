c4823ec1da70d7c80f97632ea4f2c59e28f2e557
Kazuhito Hokamura <k.hokamura@gmail.com>
2019-06-19 12:01:40 +0900

    Cleanup install scripts

--- lib/install/javascript/src/greeter.ts
+++ lib/install/javascript/src/greeter.ts
@@ -1,3 +1,3 @@
-export function greet(name: string): string {
-  return `Hello ${name}!`;
+export function hello(name: string): void {
+  console.log(`Hello ${name}!`);
 }
--- lib/install/template.rb
+++ lib/install/template.rb
@@ -2,23 +2,16 @@ puts "Copying config files"
 copy_file "#{__dir__}/config/simpacker.yml", "config/simpacker.yml"
 copy_file "#{__dir__}/root/webpack.config.js", "webpack.config.js"
 copy_file "#{__dir__}/root/tsconfig.json", "tsconfig.json"
-copy_file "#{__dir__}/root/package.json", "package.json"
 
-if Dir.exists?(Simpacker.config.source_path)
+unless File.exist?('package.json')
+  copy_file "#{__dir__}/root/package.json", "package.json"
+end
+
+if Dir.exist?('app/javascript')
   say "The JavaScript app source directory already exists"
 else
   say "Creating JavaScript app source directory"
-  directory "#{__dir__}/javascript", Simpacker.config.source_path
-end
-
-if File.exists?(".gitignore")
-  append_to_file ".gitignore", <<-EOS
-/public/packs
-/public/packs-test
-/node_modules
-yarn-debug.log*
-.yarn-integrity
-EOS
+  directory "#{__dir__}/javascript", 'app/javascript'
 end
 
 dep_packages = %w(
@@ -29,7 +22,7 @@ dep_packages = %w(
   ts-loader
 ).join(' ')
 
-say "Installing dev server for live reloading"
-run "yarn add --dev #{dep_packages}"
+say "Installing npm packages"
+run "npm install --save-dev #{dep_packages}"
 
 say "Simpacker successfully installed 🎉 🍰", :green
